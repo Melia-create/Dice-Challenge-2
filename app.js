3778c60054fad623b0c0c0cd9fce5529a84ebb19
Amelia <amelia.riding@Sofology.co.uk>
2021-02-07 20:06:34 +0000

    Added more touches to HTML and styling, now adding JS bit by bit

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 let newGame = document.querySelector("#ngbtn")
+let rollBtn = document.querySelector("#rolldicebtn")
+let holdBtn = document.querySelector("#holdbtn")
+let diceImg = document.querySelector ("#die")
+
+newGame.addEventListener("click", () => {
+   rollBtn.style.visibility = "visible";
+   holdBtn.style.visibility = "visible";
+   diceImg.style.visibility = "visible";
+});
+
+
+// function rollDice () {
+//     let rollResult = Math.ceil(Math.random() * 6);
+//     if (rollResult ==)
+// }
+
+
+diceImg.innerHTML = '<img src =img/dice' + rollResult + '.png'
